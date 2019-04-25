f77144312ac91a2ac392afc42b8953b51812bfdb
Josh Webb <joshwebb42@gmail.com>
2019-04-25 09:06:55 -0500

    unsuccessful button

--- src/scripts/keys.js
+++ src/scripts/keys.js
@@ -1,2 +1,3 @@
 const eventKey = "V6VHCMBYIDY5NSMFU3FK";
-const concertKey = "tE9GuWo4wLuJFK8wwpUFIHPujVrriL7E"
+const concertKey = "tE9GuWo4wLuJFK8wwpUFIHPujVrriL7E";
+const resKey = "009575da97a7b2eff9ebe08ad48a9d40";
